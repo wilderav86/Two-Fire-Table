aaadfdbd1b9e4c2e79a18eb5b49b10e2fcf4cf25
Adam Wilder <wilderav86@gmail.com>
2021-12-20 19:00:10 -0800

    added styling

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
-
 import Header from "./Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../style.css";
 
 const Layout = ({ children }) => {
   return (
