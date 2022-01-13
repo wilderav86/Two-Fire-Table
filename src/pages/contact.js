371235d5e68aac55aa00e2d500cf8300e1fc07d3
Adam Wilder <wilderav86@gmail.com>
2022-01-13 14:12:00 -0800

    added form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -16,16 +16,27 @@ const Contact = () => {
         >
           <input type="hidden" name="form-name" value="contact" />
           <Form.Group required className="mb-3" controlId="formName">
-            <Form.Control type="name" placeholder="Name" />
+            <Form.Control type="name" placeholder="Name" name="name" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control required type="email" placeholder="Email" />
+            <Form.Control
+              required
+              type="email"
+              placeholder="Email"
+              name="email"
+            />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formEvent">
-            <Form.Control required as="textarea" type="event" rows="3" />
+            <Form.Control
+              required
+              as="textarea"
+              type="event"
+              rows="3"
+              name="event-info"
+            />
             <Form.Text className="text-muted">
               Tell us about your event
             </Form.Text>
