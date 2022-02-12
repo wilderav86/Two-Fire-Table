67ee6d86666b4d81a5f72bc2690455432b9a6669
Adam Wilder <wilderav86@gmail.com>
2022-02-11 23:57:59 -0800

    added instagram gallery page and scroll to top icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,29 @@ import React from "react";
 import headerlogo from "../images/headerlogo.jpg";
 import NewsLetterForm from "./NewsLetterForm";
 import footerBadge from "../images/footerBadge.jpg";
+import { motion } from "framer-motion";
+
+const handleScrollToTop = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <img className="footer-logo" src={headerlogo} alt="logo" />
       <NewsLetterForm />
-      <img className="footer-badge" src={footerBadge} alt="badge" />
+      <motion.img
+        className="footer-badge"
+        src={footerBadge}
+        alt="badge"
+        onClick={handleScrollToTop}
+        whileHover={{
+          scale: 1.1,
+          transition: { duration: 0.2 },
+        }}
+        whileTap={{ scale: 0.9 }}
+      />
     </div>
   );
 };
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -6,23 +6,22 @@ import Layout from "../components/Layout";
 import PageFadeIn from "../animations/PageFadeIn";
 
 const Gallery = ({ data }) => {
-  const { edges } = data.allFile;
+  const { nodes } = data.allInstagramContent;
+
   return (
     <Layout>
       <PageFadeIn>
         <div className="gallery-page-container">
           <Container className="gallery-container">
             <Carousel className="carousel" touch={true}>
-              {edges.map((image) => {
-                console.log(image);
+              {nodes.map((image) => {
+                const { gatsbyImageData, id } = image.localFile.childImageSharp;
+
                 return (
-                  <Carousel.Item
-                    align="center"
-                    key={image.node.childImageSharp.id}
-                  >
+                  <Carousel.Item align="center" key={id}>
                     {
                       <GatsbyImage
-                        image={image.node.childImageSharp.gatsbyImageData}
+                        image={gatsbyImageData}
                         alt="carousel image"
                       />
                     }
@@ -40,20 +39,74 @@ const Gallery = ({ data }) => {
 export default Gallery;
 
 export const query = graphql`
-  query GalleryPageData {
-    allFile(filter: { relativeDirectory: { regex: "/carousel/" } }) {
-      edges {
-        node {
+  query InstagramGalleryData {
+    allInstagramContent(limit: 20) {
+      nodes {
+        localFile {
           childImageSharp {
             gatsbyImageData(
-              width: 800
+              placeholder: TRACED_SVG
               height: 800
-              transformOptions: { cropFocus: CENTER }
+              width: 800
+              formats: WEBP
+              webpOptions: { quality: 50 }
             )
             id
           }
         }
+        media_url
       }
     }
   }
 `;
+//   const { edges } = data.allFile;
+//   return (
+//     <Layout>
+//       <PageFadeIn>
+//         <div className="gallery-page-container">
+//           <Container className="gallery-container">
+//             <Carousel className="carousel" touch={true}>
+//               {edges.map((image) => {
+//                 console.log(image);
+//                 return (
+//                   <Carousel.Item
+//                     align="center"
+//                     key={image.node.childImageSharp.id}
+//                   >
+//                     {
+//                       <GatsbyImage
+//                         image={image.node.childImageSharp.gatsbyImageData}
+//                         alt="carousel image"
+//                       />
+//                     }
+//                   </Carousel.Item>
+//                 );
+//               })}
+//             </Carousel>
+//           </Container>
+//         </div>
+//       </PageFadeIn>
+//     </Layout>
+//   );
+// };
+
+// export default Gallery;
+
+// export const query = graphql`
+//   query GalleryPageData {
+//     allFile(filter: { relativeDirectory: { regex: "/carousel/" } }) {
+//       edges {
+//         node {
+//           childImageSharp {
+//             gatsbyImageData(
+//               width: 800
+//               height: 800
+//               transformOptions: { cropFocus: CENTER }
+//             )
+//             id
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
